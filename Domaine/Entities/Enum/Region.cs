791ab67561a2b96b4e0f06f6f6fb2c3fe6986d67
houssemnabouli@gmail.com <21754318h>
2021-03-22 17:09:56 +0100

    balkchi

---
+++ Domaine/Entities/Enum/Region.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourMe.Data.Entities.Enum
+{
+public enum Region
+    {
+        Zaghouan,
+        Tunis,
+        Tozeur, Sousse, Tataouine, Siliana, SidiBouzid, Sfax, Nabeul, Monastir, Medenine, Manouba, Mahdia,kef, Kebili, Kasserine, Kairouan, Jendouba,Gabès,Gafsa, Bizerte,Béja, BenArous, Ariana
+
+    }
+}
