287028da613823b0cc84997e50da7f17d45e949b
houssemnabouli@gmail.com <21754318h>
2021-03-22 15:33:58 +0100

    attribut+code

--- Domaine/Entities/Utilisateur.cs
+++ Domaine/Entities/Utilisateur.cs
@@ -5,11 +5,14 @@ using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Identity;
+using TourMe.Data.Entities.Enum;
 
 namespace Domaine.Entities
 {
     public class Utilisateur :IdentityUser
     {
+        public string NickName { get; set; }
+        public  Region region { get; set; }
         public int carte { get; set; }
        
         public String Nom { get; set; }
@@ -20,9 +23,12 @@ namespace Domaine.Entities
 
         public int? Telephone { get; set; }
         public String ProfilePhoto { get; set; }
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime BirthDate { get; set; }
        
-        public String BirthDate { get; set; }
         public  String Adresse { get; set; }
+        public List<string> Interet { get; set; }
+        public Gender
 
     }
 }
