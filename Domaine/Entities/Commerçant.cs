225a1bc86e9a11353d95595e55dd9a38f125e4a4
houssemnabouli@gmail.com <21754318h>
2021-03-22 16:37:48 +0100

    xD

---
+++ Domaine/Entities/Commerçant.cs
@@ -0,0 +1,22 @@
+﻿using Domaine.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TourMe.Data.Entities.Enum;
+
+namespace Domaine.Entities
+{
+   public  class Commerçant:Utilisateur
+    {
+
+        public string Type { get; set; }
+        public string DomainActivite { get; set; }
+        public string FormeJuridique { get; set; }
+        public int EffectFemme { get; set; }
+        public int EffectHomme { get; set; }
+        public string SituationEntreprise { get; set; }
+        public string NomGerant { get; set; }
+        public string Patente { get; set; }
+        public string Secteur { get; set; }
+    }
+}
