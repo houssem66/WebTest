1dd4257a66ba6dac357c2247f90220c833d86e48
pinkyy <pinkyy@PINKY>
2021-03-25 16:56:51 +0100

    NameSpaceChange

--- WebApplication2/Models/LoginViewModel.cs
+++ WebApplication2/Models/LoginViewModel.cs
@@ -6,7 +6,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace WebApplication2.Models
+namespace TourMe.Web.Models
 {
     public class LoginViewModel
     {
