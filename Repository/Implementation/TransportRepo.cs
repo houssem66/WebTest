2ad334efa527040ddfa660f62f9475e2398f5dae
pinkyy <pinkyy@PINKY>
2021-05-05 23:18:20 +0100

    OneToOne

---
+++ Repository/Implementation/TransportRepo.cs
@@ -0,0 +1,16 @@
+﻿using Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Implementation
+{
+   public class TransportRepo : ITransportRepo
+    {
+       
+
+
+    }
+}
