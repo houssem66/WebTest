c0c777c55e96769c0f2251cce4156be541fe63df
houssemnabouli@gmail.com <21754318h>
2021-05-06 15:49:23 +0100

    premier commit

---
+++ Domaine/Entities/ServiceLogment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TourMe.Data.Entities.Enum;
+
+namespace TourMe.Data.Entities
+{
+  public  class ServiceLogment
+    {[Key]
+        public int Id { get; set; }
+        public Category Category { get; set; }
+        public string Images{ get; set; }
+        public string Documents { get; set; }
+        public decimal PrixParNuit { get; set; }
+        public string Adresse { get; set; }
+        public string Titre { get; set; }
+
+
+        public Fournisseur Fournisseur { get; set; }
+    }
+}
---
+++ Repository/Implementation/FournisseurRepo.cs
@@ -0,0 +1,38 @@
+﻿using Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TourMe.Data;
+using TourMe.Data.Entities;
+
+namespace Repository.Implementation
+{
+ public   class FournisseurRepo : IFournisseurRepo
+    {
+        private readonly TourMeContext dbContext;
+        private readonly IGenericRepository<Fournisseur> genericRepoFournisseur;
+
+        public FournisseurRepo(TourMeContext dbContext, IGenericRepository<Fournisseur> _GenericRepoFournisseur)
+        {
+            this.dbContext = dbContext;
+            genericRepoFournisseur = _GenericRepoFournisseur;
+        }
+
+        public async Task<string> Delete(Fournisseur entity)
+        {
+            try
+            {
+
+                await genericRepoFournisseur.DeleteAsync(entity.Id);
+            }
+            catch (Exception)
+            {
+                return "fail";
+
+            }
+            return "success";
+        }
+    }
+}
