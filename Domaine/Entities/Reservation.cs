8991cf780deda39be2628250707411788b5d2ff4
houssemnabouli@gmail.com <21754318h>
2021-05-07 15:38:49 +0100

    Reservation :class

---
+++ Domaine/Entities/Reservation.cs
@@ -0,0 +1,23 @@
+﻿using Domaine.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourMe.Data.Entities
+{
+   public class Reservation
+    {
+        public string UtilisateurId { get; set; }
+        public virtual Utilisateur utilisateur { get; set; }
+
+        public int ExperienceId { get; set; }
+        public virtual Experience experience { get; set; }
+
+        public DateTime date { get; set; }
+        public int NbrReservation { get; set; }
+        public decimal tariff { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
