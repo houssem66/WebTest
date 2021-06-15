17d8afc69706676377b5b0ea1acee2f6b6b251fe
houssemnabouli@gmail.com <21754318h>
2021-06-15 15:29:04 +0100

    partie 1 Events

--- WebApplication2/Models/CommercentViewModel.cs
+++ WebApplication2/Models/CommercentViewModel.cs
@@ -29,8 +29,8 @@ namespace TourMe.Web.Models
         public string SituationEntreprise { get; set; }
         public string NomGerant { get; set; }
         public string Patente { get; set; }
-        [RegularExpression(@"([0-9]+)", ErrorMessage = "Must be a Number.")]
-        [StringLength(8, ErrorMessage = "The {0}  cannot exceed {1} characters. ")]
+        [RegularExpression(@"^[0-9]{8}[A-Za-z]$", ErrorMessage = "Must be a In this format 12345678X.")]
+        [StringLength(9, ErrorMessage = "The {0}  cannot exceed {1} characters. ")]
         public string Identifiant_fiscale { get; set; }
         [Display(Name = "Secteur")]
         public string Secteur { get; set; }
@@ -55,14 +55,12 @@ namespace TourMe.Web.Models
         public string PhoneNumberCountryCode { get; set; }
         public IFormFile FileP { get; set; }
         public List<IFormFile> Documents { get; set; }
-        [RegularExpression(@"([0-9]+)", ErrorMessage = "Must be a Number.")]
-        [StringLength(4, ErrorMessage = "The {0}  cannot exceed {1} characters. ")]
-        public string CodePostale { get; set; }
+        
+        public int CodePostale { get; set; }
         public Region region { get; set; }
         public string Adresse { get; set; }
-        [RegularExpression(@"([0-9]+)", ErrorMessage = "Must be a Number.")]
-        [StringLength(10, ErrorMessage = "The {0}  cannot exceed {1} characters. ")]
-        public string NumCnss { get; set; }
+        
+        public long NumCnss { get; set; }
 
     }
 }
